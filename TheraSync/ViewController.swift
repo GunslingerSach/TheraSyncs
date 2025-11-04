71522869e97aeb6fa26af3fc86d9f1f35e377adb
GunslingerSach <sachinsingh1511@gmail.com>
2025-11-04 15:15:05 +0530

    Initial Commit

---
+++ TheraSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheraSync
+//
+//  Created by user@54 on 04/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TheraSyncTests/TheraSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheraSyncTests.swift
+//  TheraSyncTests
+//
+//  Created by user@54 on 04/11/25.
+//
+
+import Testing
+@testable import TheraSync
+
+struct TheraSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
